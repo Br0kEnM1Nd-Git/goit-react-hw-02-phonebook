d3de419fc8b21f3a388ccdc0c670dbf4403632d0
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-11-13 00:12:05 +0200

    fix

--- src/components/PhonebookList/PhonebookFilter.jsx
+++ src/components/PhonebookList/PhonebookFilter.jsx
@@ -1,9 +1,17 @@
+import { FilterForm } from './PhonebookList.styled';
+
 const PhonebookFilter = ({ filter, handleChange }) => {
   return (
-    <>
-      <span>Find contacts:</span>
-      <input type="text" name="filter" value={filter} onChange={handleChange} />
-    </>
+    <FilterForm>
+      <label htmlFor="filter">Find contacts:</label>
+      <input
+        type="text"
+        name="filter"
+        id="filter"
+        value={filter}
+        onChange={handleChange}
+      />
+    </FilterForm>
   );
 };
 
--- src/components/PhonebookList/PhonebookList.styled.js
+++ src/components/PhonebookList/PhonebookList.styled.js
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+
+const FilterForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 12px;
+  & > label {
+    font-size: 36px;
+    width: 300px;
+  }
+  & > input {
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    width: 250px;
+    border: 0.1px solid grey;
+    &:focus-visible {
+      outline: 0;
+      background-color: rgba(0, 0, 155, 0.1);
+    }
+  }
+`;
+
+export { FilterForm };
